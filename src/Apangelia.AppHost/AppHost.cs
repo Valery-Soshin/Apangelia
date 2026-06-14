39a620177652e4819c2f921b15ff489aeea883a5
Валерий Сошин <105991605+Valery-Soshin@users.noreply.github.com>
2026-06-14 22:13:58 +0300

    feat(apphost): add smee tunnel

--- src/Apangelia.AppHost/AppHost.cs
+++ src/Apangelia.AppHost/AppHost.cs
@@ -6,8 +6,16 @@ var password = builder.AddParameter("password", "postgres", secret: true);
 var postgres = builder.AddPostgres("apangelia-postgres", username, password,port: 5434)
     .WithDataVolume(isReadOnly: false);
 
-builder.AddProject<Projects.Apangelia_WebApi>("apangelia-webapi")
+var webapi = builder.AddProject<Projects.Apangelia_WebApi>("apangelia-webapi")
     .WithReference(postgres)
     .WaitFor(postgres);
+
+var smee = builder.AddExecutable(
+    name: "smee-github",
+    command: "smee",
+    workingDirectory: builder.AppHostDirectory,
+    "-u", builder.Configuration["Smee:FromUrl"]!,
+    "-t", builder.Configuration["Smee:ToUrl"]!)
+    .WaitFor(webapi);
 
 builder.Build().Run();
